a6b0f4aa5454de10547697067a0fbae1a6e40322
ArsalanTauseef <142938248+ArsalanTauseef@users.noreply.github.com>
2024-07-23 08:38:19 +0500

    new

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 import './Header.css'
 
 export const Header = () => {
+
+    const [scrolled, setScrolled] = useState(false);
+
+    const handleScroll = () => {
+    const scrollY = window.scrollY;
+    // 6em in pixels (assuming 1em = 16px, 6em = 96px)
+    if (scrollY > 96) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+    };
+
+    useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+    }, []);
+
   return (
     <>
-    <header>
+    <header id='mandatory-class' className={scrolled ? 'header-css' : ''}>
         <nav>
             <div className='nav-left'>
                 <h3>Perfect<span style={{color:'#B65F54'}}>Recipe</span></h3>
--- src/Components/Midsection/Nav Pages/Home/Home.jsx
+++ src/Components/Midsection/Nav Pages/Home/Home.jsx
@@ -15,6 +15,16 @@ export const Home = () => {
             <div></div>
         </div>
     </main>
+    <main className='upper-middle-section'>
+      <div>
+        <img src="https://media.istockphoto.com/id/1160735570/photo/healthy-vegan-meal.jpg?s=612x612&w=0&k=20&c=4D7qqrqEPubOXN-K0JPeDGsioN5A6YhgxY-PmI9EcGk=" alt="" />
+      </div>
+      <div>
+        <h1>Share Your <span style={{color:'#B65F54', cursor:'pointer'}}>Recipes</span></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae enim pharetra, venenatis nunc eget, finibus est. Proin velit </p>
+        <button>Create New Recipe </button>
+      </div>
+    </main>
     </>
   )
 }
