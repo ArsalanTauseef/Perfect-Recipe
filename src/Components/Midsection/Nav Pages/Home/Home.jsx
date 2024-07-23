1f7ffcdabd566b309219d3b2166d2fc62b24e719
ArsalanTauseef <142938248+ArsalanTauseef@users.noreply.github.com>
2024-07-23 07:56:10 +0500

    new commit

--- src/Components/Midsection/Nav Pages/Home/Home.jsx
+++ src/Components/Midsection/Nav Pages/Home/Home.jsx
@@ -9,7 +9,7 @@ export const Home = () => {
             <h1>Your Daily Dish A <span style={{color:'#B65F54'}}>Food</span> Journey</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed enim magni voluptatum pariatur odio tempora ex, hic tempore iusto ipsam?</p>
             <div><button>Sign Up</button></div>
-            <span>Do You Have Account Already? <span style={{color:'#B65F54'}}>Logn In</span></span>
+            <span>Do You Have Account Already? <span style={{color:'#B65F54', cursor:'pointer'}}>Logn In</span></span>
         </div>
         <div className='upper-div-two'>
             <div></div>
